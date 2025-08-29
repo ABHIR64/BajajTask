0438d83f2762e0eee3f525ae689566c063ff749c
ABHIR64 <bansalabhir.1964@gmail.com>
2025-08-29 12:03:07 +0530

    commit

---
+++ api/bfhl.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const serverless = require("serverless-http");
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post("/bfhl", (req, res) => {
+  const data = req.body.data || [];
+
+  const odd_numbers = data.filter(x => /^\d+$/.test(x) && parseInt(x) % 2 !== 0).map(Number);
+  const even_numbers = data.filter(x => /^\d+$/.test(x) && parseInt(x) % 2 === 0).map(Number);
+  const alphabets = data.filter(x => /^[a-zA-Z]$/.test(x)).map(x => x.toUpperCase());
+  const special_characters = data.filter(x => !/^[a-zA-Z0-9]+$/.test(x));
+  const sum = odd_numbers.concat(even_numbers).reduce((a, b) => a + b, 0);
+  const concat_string = alphabets.join("");
+
+  res.json({
+    is_success: true,
+    user_id: "john_doe_17091999",
+    email: "john@xyz.com",
+    roll_number: "ABCD123",
+    odd_numbers,
+    even_numbers,
+    alphabets,
+    special_characters,
+    sum,
+    concat_string,
+  });
+});
+
+module.exports = app;
+module.exports.handler = serverless(app);
